4e3c1b7676062232118f9773eb6bf073df7ee3a3
nishchalbhattarai1998 <nishchalbhattarai1998>
2023-04-07 08:56:58 -0400

    Initial Commit

---
+++ Nishchal-Bhattarai_INFO-6131-01-23W-Assignment2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nishchal-Bhattarai_INFO-6131-01-23W-Assignment2
+//
+//  Created by nishchal bhattarai on 2023-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nishchal-Bhattarai_INFO-6131-01-23W-Assignment2/Nishchal_Bhattarai_INFO_6131_01_23W_Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nishchal_Bhattarai_INFO_6131_01_23W_Assignment2App.swift
+//  Nishchal-Bhattarai_INFO-6131-01-23W-Assignment2
+//
+//  Created by nishchal bhattarai on 2023-04-07.
+//
+
+import SwiftUI
+
+@main
+struct Nishchal_Bhattarai_INFO_6131_01_23W_Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
